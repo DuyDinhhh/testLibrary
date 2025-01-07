7a93dc12e671dba55690fad70f85722ea96bccb4
DuyDinhhh <161542772+DuyDinhhh@users.noreply.github.com>
2025-01-07 16:01:18 +0700

    Create qualitySonarCheck.groovy

---
+++ vars/qualitySonarCheck.groovy
@@ -0,0 +1,16 @@
+def call(Map config){
+  stage('SonarQube') {
+        steps {
+            script {
+                withSonarQubeEnv(config.sonarEnv ?: 'SONAR_LATEST') {
+                    sh """
+                    ${config.scannerHome ?: '$SCANNER_HOME'}/bin/sonar-scanner \
+                    -Dsonar.projectKey=${config.BRANCH_NAME.replace('/', '_')} \
+                    -Dsonar.projectName=${config.BRANCH_NAME} \
+                    -Dsonar.java.binaries=target/classes
+                    """
+                }
+            }
+        }
+    }
+}
