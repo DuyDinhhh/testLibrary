238e16d6736e237568e401d7d345ef198f5ecab7
DuyDinhhh <161542772+DuyDinhhh@users.noreply.github.com>
2025-01-14 14:40:21 +0700

    Update qualitySonarCheck.groovy

--- vars/qualitySonarCheck.groovy
+++ vars/qualitySonarCheck.groovy
@@ -1,10 +1,11 @@
  def call() {
-        withSonarQubeEnv(env.sonarEnv ?: 'SONAR_LATEST') {
-           sh """
-           ${env.SCANNER_HOME}/bin/sonar-scanner \
-           -Dsonar.projectKey=${env.BRANCH_NAME.replace('/', '_')} \
-           -Dsonar.projectName=${env.BRANCH_NAME} \
-           -Dsonar.java.binaries=target/classes
-           """
+         withSonarQubeEnv('SONAR_LATEST') {
+               sh """
+               $SCANNER_HOME/bin/sonar-scanner \
+               -Dsonar.projectKey=${env.BRANCH_NAME.replace('/', '_')} \
+               -Dsonar.projectName=${env.BRANCH_NAME} \
+               -Dsonar.java.binaries=target/classes
+               """
+           }
        }
 }
